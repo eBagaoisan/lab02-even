2356e64da2c16f058663ada1bed2c6cd065af455
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2018-02-01 14:21:42 -0800

    First commit

---
+++ even.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int userInput;
+    bool allEven = true;
+    cout<<"Please enter a number: "<<endl;
+    while(cin>>userInput){
+        if(userInput % 2 != 0){
+            allEven = false;
+        }
+    }
+    if (allEven == true){
+        cout<<"all even"<<endl;
+    }
+    else{
+        cerr<<"not all even"<<endl;
+    }
+    return 0;
+}
